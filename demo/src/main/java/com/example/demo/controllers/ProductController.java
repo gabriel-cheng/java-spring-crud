11dd52979c346201d7ecc061c5173a1195dced58
Gabriel Carvalho <gabriel_carvalho.contato@outlook.com>
2025-03-10 18:03:02 -0300

    Inicial commit

---
+++ demo/src/main/java/com/example/demo/controllers/ProductController.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.demo.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Gabriel
+ */
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+    
+    @GetMapping
+    public ResponseEntity getAllProducts() {
+        return ResponseEntity.ok("Deu ok");
+    }
+}
