271ef76af512762d15cf90b9ca2425b72dfb078b
Gabriel Carvalho <gabriel_carvalho.contato@outlook.com>
2025-03-11 12:27:17 -0300

    chore: changing project path

--- src/main/java/com/example/demo/controllers/ProductController.java
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.example.demo.controllers;
 
 import org.springframework.http.ResponseEntity;
@@ -17,8 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/product")
 public class ProductController {
     
-    @GetMapping
+    @GetMapping("/")
     public ResponseEntity getAllProducts() {
-        return ResponseEntity.ok("Deu ok");
+        return ResponseEntity.ok("Hello, world!");
     }
+    
 }
---
+++ src/main/java/com/example/demo/domain/product/Product.java
@@ -0,0 +1,27 @@
+package com.example.demo.domain.product;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Table(name="product")
+@Entity(name="product")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Product {
+
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    
+    private String name;
+    
+    private Number price_in_cents;
+
+
+}
