dd6c516285973765957b62b942a2498fdcd9d00e
Gabriel Carvalho <gabriel_carvalho.contato@outlook.com>
2025-03-12 00:33:18 -0300

    feat: creating get one method, delete method and put method

--- src/main/java/com/example/demo/controllers/ProductController.java
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -1,12 +1,23 @@
 package com.example.demo.controllers;
 
-import com.example.demo.domain.product.Product;
-import com.example.demo.domain.product.ProductRepository;
-import com.example.demo.domain.product.RequestProduct;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.product.Product;
+import com.example.demo.domain.product.ProductRepository;
+import com.example.demo.domain.product.RequestProduct;
 
 /**
  *
@@ -26,6 +37,13 @@ public class ProductController {
         return ResponseEntity.ok(allProducts);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity getOneProduct(@PathVariable String id) {
+        Optional<Product> productFinded = repository.findById(id);
+    
+        return ResponseEntity.ok(productFinded);
+    }
+
     @PostMapping
     public ResponseEntity registerNewProduct(@RequestBody @Validated RequestProduct data) {
         Product newProduct = new Product(data);
@@ -37,9 +55,27 @@ public class ProductController {
 
     @DeleteMapping
     public ResponseEntity deleteProduct(@RequestParam String id) {
-        Object productFinded = repository.findById(id);
-    
-        return ResponseEntity.ok(productFinded);
+        repository.deleteById(id);
+        
+        return ResponseEntity.ok("deleted");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity modifyProduct(@RequestBody @Validated RequestProduct data, @PathVariable String id) {
+        Optional<Product> productFinded = repository.findById(id);
+        
+        if(productFinded.isPresent()) {
+            Product product = productFinded.get();
+
+            product.setName(data.name());
+            product.setPrice_in_cents(data.price_in_cents());
+
+            repository.save(product);
+
+            return ResponseEntity.ok("Product changed!");
+        }
+
+        return ResponseEntity.notFound().build();
     }
     
 }
